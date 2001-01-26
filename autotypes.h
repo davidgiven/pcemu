6f7353017ea13469e159798dbe7cc7dc729df5fb
Michael Hope <michaelh@juju.net.nz>
2001-01-26 05:19:31 +0000

    Added Solaris example to the Makefile. Updated the pcemurc documentation. Removed the extra autodetect.h file. Added two comments to cpu.c. Fixed up the atexit in curses.c Fixed up an initilisation bug in ems.c

--- autotypes.h
+++
@@ -1,9 +0,0 @@
-/* Automatically created file --- do not edit */
-
-typedef signed char INT8;
-typedef unsigned char UINT8;
-typedef signed short int INT16;
-typedef unsigned short int UINT16;
-typedef signed int INT32;
-typedef unsigned int UINT32;
-#define BIG_ENDIAN
